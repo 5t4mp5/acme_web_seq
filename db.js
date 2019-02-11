5a7faf973baa271e2c9a83dc5366d0cc614c6827
unknown <5t4mp5@gmail.com>
2019-02-11 16:32:54 -0500

    done with seq calls, but need to test

--- db.js
+++ db.js
@@ -52,19 +52,19 @@ function seedDb(){
                 await contact.save();
 
                 const homeContent = new Content({
-                    body: "<p>Welcome Home 1</p><p>Welcome Home 2</p>"
+                    body: '<ul class="list-group"><li class="list-group-item">Welcome Home 1<br> XOXOXO</li><li class="list-group-item">Welcome Home 2<br> XOXOXO</li></ul>'
                 });
                 await homeContent.save();
                 homeContent.setPage(1);
 
                 const employeesContent = new Content({
-                    body: '<p>Moe\nCEO</p><p>Larry\nCOO</p><p>CURLY\nCOO</p>'
+                    body: '<ul class="list-group"><li class="list-group-item">MOE<br>CEO</li><li class="list-group-item">LARRY<br>CTO</li><li class="list-group-item">CURLY<br>COO</li></ul>'
                 });
                 await employeesContent.save();
                 employeesContent.setPage(2);
 
                 const contactContent = new Content({
-                    body: '<p>Phone\n212-555-1212</p><p>Telex\n212-555-1213</p><p>FAX\n212-555-1214</p>'
+                    body: '<ul class="list-group"><li class="list-group-item">Phone<br>212-555-1212</li><li class="list-group-item">Telex<br>212-555-1213</li><li class="list-group-item">Fax<br>212-555-1214</li></ul>'
                 });
                 await contactContent.save();
                 contactContent.setPage(3);
@@ -72,4 +72,11 @@ function seedDb(){
         });
 }
 
-module.exports = {syncDataBase, seedDb};
+function getBody(pageTitle){
+    return Page.findOne({where: {title: pageTitle}})
+        .then(page => page.id)
+        .then(id => Content.findOne({where:{pageId: id}}))
+        .then(content => content.body)
+}
+
+module.exports = {syncDataBase, seedDb, getBody};
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const {syncDataBase, seedDb} = require ('./db');
+const app = require ('./app');
 
-syncDataBase()
+const port = process.env.port || 1337;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+    syncDataBase()
     .then(() => console.log('db sync done'))
     .then(() => seedDb())
     .then(() => console.log('db seed done'))
     .catch(err => err);
+});
+
+
 
 
