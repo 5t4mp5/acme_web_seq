435b210e5b619ea6ad0f3daa15ca0b0db7abb72c
Dave Siegel <dave@Admins-MBP.home>
2019-02-10 23:44:10 -0500

    db seed and sync

--- db.js
+++ db.js
@@ -21,4 +21,55 @@ async function syncDataBase(){
     await Content.sync();
 }
 
-module.exports = syncDataBase;
+function itExists(table, id){
+    return table.count({where: {id: id}})
+        .then(count => {
+            if(count > 0){
+                return true;
+            }
+            return false;
+        });
+}
+
+function seedDb(){
+    itExists(Page, 1)
+        .then(async itExists =>  {
+            if(!itExists){
+                const home = new Page({
+                    title: 'Home'
+                });
+            
+                const employees = new Page({
+                    title: 'Employees'
+                });
+            
+                const contact = new Page({
+                    title: 'Contact'
+                });
+
+                await home.save();
+                await employees.save();
+                await contact.save();
+
+                const homeContent = new Content({
+                    body: "<p>Welcome Home 1</p><p>Welcome Home 2</p>"
+                });
+                await homeContent.save();
+                homeContent.setPage(1);
+
+                const employeesContent = new Content({
+                    body: '<p>Moe\nCEO</p><p>Larry\nCOO</p><p>CURLY\nCOO</p>'
+                });
+                await employeesContent.save();
+                employeesContent.setPage(2);
+
+                const contactContent = new Content({
+                    body: '<p>Phone\n212-555-1212</p><p>Telex\n212-555-1213</p><p>FAX\n212-555-1214</p>'
+                });
+                await contactContent.save();
+                contactContent.setPage(3);
+            }
+        });
+}
+
+module.exports = {syncDataBase, seedDb};
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-const syncDataBase = require ('./db');
+const {syncDataBase, seedDb} = require ('./db');
 
 syncDataBase()
     .then(() => console.log('db sync done'))
+    .then(() => seedDb())
+    .then(() => console.log('db seed done'))
     .catch(err => err);
+
+
